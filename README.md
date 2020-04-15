b1af0469bc5d0333023439b572d9bb67a010a31c
Marco Eidinger <marco.eidinger@sap.com>
2020-04-15 11:58:18 -0700

    init framework project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # obj-framework-in-swift-framework
+
+1. Create Framework project with Xcode
---
+++ SwiftyCrashReporter/SwiftyCrashReporter/SwiftyCrashReporter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCrashReporter.h
+//  SwiftyCrashReporter
+//
+//  Created by Eidinger, Marco on 4/15/20.
+//  Copyright © 2020 Eidinger, Marco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyCrashReporter.
+FOUNDATION_EXPORT double SwiftyCrashReporterVersionNumber;
+
+//! Project version string for SwiftyCrashReporter.
+FOUNDATION_EXPORT const unsigned char SwiftyCrashReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCrashReporter/PublicHeader.h>
+
+
